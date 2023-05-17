798c78d0f6265941e3efc264f26ccd632ea183fe
DavidJKTofan <davidtofan@gmail.com>
2023-05-17 08:51:50 +0200

    Update client.js

--- client.js
+++ client.js
@@ -2,49 +2,58 @@ const resultContainer = document.getElementById("resultContainer");
 
 // Client application code
 async function makeJsonRpcRequest(method, params) {
-  const url = "https://grpc.automatic-demo.com/";
+    const url = "https://grpc.automatic-demo.com/";
 
-  const rpcRequest = {
-    jsonrpc: "2.0",
-    method: method,
-    params: params,
-    id: 1, // Unique request ID
-  };
+    const rpcRequest = {
+        jsonrpc: "2.0",
+        method: method,
+        params: params,
+        id: 1, // Unique request ID
+    };
 
-  const response = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify(rpcRequest),
-    headers: { "Content-Type": "application/json" },
-  });
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(rpcRequest),
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
 
-  const rpcResponse = await response.json();
+    const rpcResponse = await response.json();
 
-  if ("error" in rpcResponse) {
-    throw new Error(rpcResponse.error.message);
-  }
+    if ("error" in rpcResponse) {
+        throw new Error(rpcResponse.error.message);
+    }
 
-  return rpcResponse.result;
+    return rpcResponse.result;
 }
 
 // Client-side code (continued)
 async function runDemo() {
-  try {
-    //   const method = "method1";
-    //   const params = { param1: "value1", param2: "value2" };
-    //   const result = await makeJsonRpcRequest(method, params);
-    //   console.log("Result:", result);
+    try {
+        //   const method = "method1";
+        //   const params = { param1: "value1", param2: "value2" };
+        //   const result = await makeJsonRpcRequest(method, params);
+        //   console.log("Result:", result);
 
-    // Generate random parameters
-    const params = generateRandomParams();
-    // Randomly select method1 or method2
-    const method = Math.random() < 0.5 ? "method1" : "method2";
-    const result = await makeJsonRpcRequest(method, params);
-    console.log("makeJsonRpcRequest: ", result)
-    // Access the result and cfData properties from the response
-    const { Newresult, cfData } = result;
-    // Update the result in the HTML div
-    const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML += `
+        // Generate random parameters
+        const params = generateRandomParams();
+        // Randomly select method1 or method2
+        const method = Math.random() < 0.5 ? "method1" : "method2";
+        const response = await makeJsonRpcRequest(method, params);
+
+        if (response.ok) {
+
+            const result = await response.json();
+            console.log("makeJsonRpcRequest: ", result)
+            // Access the result and cfData properties from the response
+            const {
+                Newresult,
+                cfData
+            } = result;
+            // Update the result in the HTML div
+            const resultDiv = document.getElementById("result");
+            resultDiv.innerHTML += `
       <p><strong>Method:</strong> ${method}</p>
       <p><strong>Params:</strong> ${JSON.stringify(params)}</p>
       <p><strong>Result:</strong> ${Newresult.result}</p>
@@ -52,20 +61,26 @@ async function runDemo() {
       <p><strong>City:</strong> ${cfData.city}</p>
       <hr>
     `;
-    console.log("Method:", method);
-    console.log("Params:", params);
-    console.log("Result:", Newresult.result);
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
+            console.log("Method:", method);
+            console.log("Params:", params);
+            console.log("Result:", Newresult.result);
+        } else {
+            console.error('Error:', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
 }
 
 function generateRandomParams() {
-  // Generate random parameter values as needed
-  const param1 = Math.random().toString(36).substr(2, 8);
-  const param2 = Math.random().toString(36).substr(2, 8);
+    // Generate random parameter values as needed
+    const param1 = Math.random().toString(36).substr(2, 8);
+    const param2 = Math.random().toString(36).substr(2, 8);
 
-  return { param1, param2 };
+    return {
+        param1,
+        param2
+    };
 }
 
 // runDemo();
